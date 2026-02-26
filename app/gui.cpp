1fcbda190e791b41af657f38d2f5a103921a4189
mokomarovskii <MOKomarovskii23@codingburgas.bg>
2026-02-26 20:31:47 +0200

    Add gui preset

---
+++ app/gui.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <cfloat>
+#include <cstdio>
+#include <string>
+#include <vector>
+
+#include "database.h"
+
+#if defined(TIMELINE_IMGUI_GUI)
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
