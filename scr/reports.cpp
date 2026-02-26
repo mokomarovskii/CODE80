bd97ba749040eb1b38566ffb67f8899920fd6f71
VMTarnavskii23 <VMTarnavskii23@codingburgas.bg>
2026-02-26 20:05:38 +0200

    Add functional for reports

--- scr/reports.cpp
+++ scr/reports.cpp
@@ -2,3 +2,52 @@
 #include "reports.h"
 using namespace std;
 
+void reportAllTitles(Node* head) {
+    cout << "\n[REPORT 1] All titles:\n";
+    if (head == nullptr) {
+        cout << "No data.\n";
+        return;
+    }
+
+    Node* current = head;
+    while (current != nullptr) {
+        cout << "- " << current->data.title << "\n";
+        current = current->next;
+    }
+}
+
+void reportByTheme(Node* head, const string& theme) {
+    cout << "\n[REPORT 2] Events by theme: " << theme << "\n";
+    bool hasData = false;
+
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data.theme.find(theme) != string::npos) {
+            cout << "- " << current->data.title << " (" << current->data.date.month << "." << current->data.date.year << ")\n";
+            hasData = true;
+        }
+        current = current->next;
+    }
+
+    if (!hasData) {
+        cout << "No events found.\n";
+    }
+}
+
+void reportBulgarianWins(Node* head) {
+    cout << "\n[REPORT 3] Events with Bulgaria victory:\n";
+    bool hasData = false;
+
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data.result.find("Bulg") != string::npos && current->data.result.find("victor") != string::npos) {
+            cout << "- " << current->data.title << " | " << current->data.result << "\n";
+            hasData = true;
+        }
+        current = current->next;
+    }
+
+    if (!hasData) {
+        cout << "No events found.\n";
+    }
+}
