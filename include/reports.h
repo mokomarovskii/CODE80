2fa60322825749f1870122887b291f0312a6d820
VMTarnavskii23 <VMTarnavskii23@codingburgas.bg>
2026-02-26 19:48:33 +0200

    Add logic for input H

---
+++ include/reports.h
@@ -0,0 +1,10 @@
+#ifndef REPORTS_H
+#define REPORTS_H
+
+#include "event_list.h"
+
+void reportAllTitles(Node* head);
+void reportByTheme(Node* head, const string& theme);
+void reportBulgarianWins(Node* head);
+
+#endif
