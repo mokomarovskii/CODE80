ee0371dbcfb2119e0ad2326c2225f31c1db570c5
VMTarnavskii23 <VMTarnavskii23@codingburgas.bg>
2026-02-26 20:03:39 +0200

    Add logic for event list

--- scr/event_list.cpp
+++ scr/event_list.cpp
@@ -3,3 +3,166 @@
 #include "event_list.h"
 using namespace std;
 
+EventList::EventList() {
+    head = nullptr;
+}
+
+EventList::~EventList() {
+    clear();
+}
+
+void EventList::clear() {
+    Node* current = head;
+    while (current != nullptr) {
+        Node* toDelete = current;
+        current = current->next;
+        delete toDelete;
+    }
+    head = nullptr;
+}
+
+bool EventList::isEmpty() const {
+    return head == nullptr;
+}
+
+void EventList::addFirst(const Event& event) {
+    if (head != nullptr) {
+        return;
+    }
+    head = new Node{event, nullptr};
+}
+
+void EventList::addAtStart(const Event& event) {
+    Node* newNode = new Node{event, head};
+    head = newNode;
+}
+
+void EventList::addAtEnd(const Event& event) {
+    Node* newNode = new Node{event, nullptr};
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+void EventList::addSortedByDate(const Event& event, bool ascending) {
+    Node* newNode = new Node{event, nullptr};
+
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    auto isBefore = [ascending](const Event& left, const Event& right) {
+        if (left.date.year != right.date.year) {
+            return ascending ? left.date.year < right.date.year : left.date.year > right.date.year;
+        }
+        return ascending ? left.date.month < right.date.month : left.date.month > right.date.month;
+    };
+
+    if (isBefore(event, head->data)) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr && !isBefore(event, current->next->data)) {
+        current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+bool EventList::editById(int id, const Event& updatedEvent) {
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data.id == id) {
+            current->data = updatedEvent;
+            current->data.id = id;
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+bool EventList::deleteById(int id) {
+    if (head == nullptr) {
+        return false;
+    }
+
+    if (head->data.id == id) {
+        Node* oldHead = head;
+        head = head->next;
+        delete oldHead;
+        return true;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr && current->next->data.id != id) {
+        current = current->next;
+    }
+
+    if (current->next == nullptr) {
+        return false;
+    }
+
+    Node* toDelete = current->next;
+    current->next = toDelete->next;
+    delete toDelete;
+    return true;
+}
+
+Node* EventList::findByDate(int year, int month) const {
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data.date.year == year && (month == 0 || current->data.date.month == month)) {
+            return current;
+        }
+        current = current->next;
+    }
+    return nullptr;
+}
+
+Node* EventList::findByTheme(const string& theme) const {
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data.theme == theme) {
+            return current;
+        }
+        current = current->next;
+    }
+    return nullptr;
+}
+
+Node* EventList::getHead() const {
+    return head;
+}
+
+void EventList::printAll() const {
+    if (head == nullptr) {
+        cout << "List is empty.\n";
+        return;
+    }
+
+    cout << "\n=== Historical events ===\n";
+    Node* current = head;
+    while (current != nullptr) {
+        const Event& e = current->data;
+        cout << "ID: " << e.id << " | "
+             << setw(2) << setfill('0') << e.date.month << "." << e.date.year << setfill(' ')
+             << " | " << e.title << " | Theme: " << e.theme
+             << " | Place: " << e.place
+             << " | Leader: " << e.leader
+             << " | Result: " << e.result << "\n";
+        current = current->next;
+    }
+}
